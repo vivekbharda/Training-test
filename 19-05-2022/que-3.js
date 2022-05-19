c8b0024560b69dd108c37c3d7824e32f8dd53184
bhardavivek <vivek@raoinformationtechnology.com>
2022-05-19 10:25:58 +0530

    que-3

---
+++ 19-05-2022/que-3.js
@@ -0,0 +1,14 @@
+var grade='D';  
+var result = 25;  
+switch(grade)  
+{  
+case'A':  
+        result +="10";  
+case'B':  
+        result +="9";  
+case'C':  
+        result +="8";  
+case'D' : 
+        result +="6";  
+}  
+console.log(result); 
